16a764fea5a86d94929eb16e7b92db26bf16fd5d
oliverck <oliver_ck@outlook.com>
2023-03-12 20:31:40 +0800

    config request interval

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from wechat_monitor import WechatMonitor
 
 def main(args):
     wm = WechatMonitor(args.config, output_path= args.output)
-    wm.get_newest()
-    wm.listen_wechats()
+    #wm.get_newest()
+    wm.listen_wechats(itervals=(args.time_accout, args.time_team))
 
 
 if __name__ == '__main__':
@@ -20,6 +20,9 @@ if __name__ == '__main__':
     # 添加参数
     parser.add_argument('--config', '-f', type=str, help='config path', default='./tmp/cfg.yml')
     parser.add_argument('--output', '-o', type=str, help='ourput path', default='./tmp/articles.xlsx')
+    parser.add_argument('--time_accout', '-ta', type=int, help='每个公众号的访问间隔，默认60s', default=60)
+    parser.add_argument('--time_team', '-tt', type=int, help='每组公众号的访问间隔，默认3600s', default=3600)
+
 
     # 解析参数
     args = parser.parse_args()
