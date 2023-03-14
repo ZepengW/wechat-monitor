b9d8db056283c68f5f407df3806906d707299e1e
oliverck <oliver_ck@outlook.com>
2023-03-14 13:43:50 +0800

    do not support hyperlink

--- excel_io.py
+++ excel_io.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import openpyxl
+from copy import copy
 from collections import defaultdict
 from openpyxl.styles import Font, colors
 import datetime
@@ -34,6 +35,7 @@ def get_history_aid(excel_path):
     return data_dict
 
 
+
 def write_to_excel(file_path, data, sheet_name = 'Sheet'):
     """将数据写入Excel文件"""
     # 检查文件是否存在，如果不存在则创建一个新的Excel文件
@@ -56,18 +58,10 @@ def write_to_excel(file_path, data, sheet_name = 'Sheet'):
         headers = list(data[0].keys())
         for col, header in enumerate(headers, start=1):
             new_sheet.cell(row=1, column=col, value=header)
-        # 将列宽调整为自适应列宽
+        # 调整初始化列宽
         for col in new_sheet.columns:
-            max_length = 0
             column = col[0].column_letter  # 获取列字母
-            for cell in col:
-                try:
-                    if len(str(cell.value)) > max_length:
-                        max_length = len(cell.value)
-                except:
-                    pass
-            adjusted_width = (max_length + 2) * 1.2  # 为每个单元格添加留白，乘以1.2是为了更好的显示
-            new_sheet.column_dimensions[column].width = adjusted_width
+            new_sheet.column_dimensions[column].width = 20
     else:
         for col in range(1, new_sheet.max_column + 1):
             headers.append(new_sheet.cell(row=1, column=col).value)
@@ -91,15 +85,21 @@ def write_to_excel(file_path, data, sheet_name = 'Sheet'):
                 break
         time_list.insert(row_to_insert - 2, datetime.datetime.strptime(item['发布时间'], '%Y-%m-%d %H:%M:%S'))
         new_sheet.insert_rows(row_to_insert)
+
+
         for col, value in item.items():
             col_index = headers.index(col) + 1
-            if col == '文章链接':
-                # 如果该列是“文章链接”，则将单元格设置为超链接
-                cell = new_sheet.cell(row=row_to_insert, column=col_index, value="链接")
-                cell.font = Font(color=colors.BLUE, underline='single')
-                cell.hyperlink = value
-            else:
-                new_sheet.cell(row=row_to_insert, column=col_index, value=value)
+            new_sheet.cell(row=row_to_insert, column=col_index, value=value)
+            # if col == '文章链接':
+            #     # 如果该列是“文章链接”，则将单元格设置为超链接
+            #     cell = new_sheet.cell(row=row_to_insert, column=col_index, value=value, hyperlink=value)
+            #     cell.font = Font(color=colors.BLUE, underline='single')
+            #     # Copy the hyperlink from the row above
+            #     source_cell = worksheet.cell(row=row_idx-1, column=col_idx)
+            #     if source_cell.hyperlink is not None:
+            #         cell._hyperlink = copy(source_cell.hyperlink)
+            # else:
+            #     new_sheet.cell(row=row_to_insert, column=col_index, value=value)
     
     # 保存Excel文件
     workbook.save(file_path)
@@ -179,16 +179,14 @@ def read_all_columns(file_path, column_name):
 if __name__ == '__main__':
     #search_aid('/Users/wangzepeng/Downloads/test.xlsx')
     data = [
-    {"公众号": "A22A1", "发布时间": "2023-03-09 11:28:39", "文章标题": "12", '文章链接':'www.baidu.com', '文章ID':'12'},
-    {"公众号": "A22A1", "发布时间": "2023-04-09 11:28:39", "文章标题": "2332", '文章链接':'www.baidu.com', '文章ID':'12'},
-    {"公众号": "A22A1", "发布时间": "2023-03-10 11:28:39", "文章标题": "12132", '文章链接':'www.baidu.com', '文章ID':'12'},
-    ]
-    data2 = [
-    {"公众号": "CCCC", "发布时间": "2023-03-09 11:30:39", "文章标题": "12", '文章链接':'www.baidu.com', '文章ID':'12'},
-    {"公众号": "CCCC", "发布时间": "2023-04-09 11:48:39", "文章标题": "2332", '文章链接':'www.baidu.com', '文章ID':'12'},
-    {"公众号": "CCCC", "发布时间": "2023-05-10 11:28:39", "文章标题": "12132", '文章链接':'www.baidu.com', '文章ID':'12'},
+    {"公众号": "A22A1", "发布时间": "2023-03-09 11:28:39", "文章标题": "baidu", '文章链接':'http://www.baidu.com', '文章ID':'12'},
+    {"公众号": "A22A1", "发布时间": "2023-04-09 11:28:39", "文章标题": "google", '文章链接':'http://www.google.com', '文章ID':'12'},
+    {"公众号": "A22A1", "发布时间": "2023-04-19 11:28:39", "文章标题": "ali", '文章链接':'http://www.alibaba.com', '文章ID':'12'},
     ]
     write_to_excel('./test.xlsx', data)
+    data2 = [
+    {"公众号": "A22A1", "发布时间": "2023-03-09 11:30:39", "文章标题": "shuiyuan", '文章链接':'http://www.shuiyuan.com', '文章ID':'12'},
+    {"公众号": "A22A1", "发布时间": "2023-04-09 12:28:39", "文章标题": "weixin", '文章链接':'http://www.wechat.com', '文章ID':'12'},    ]
     write_to_excel('./test.xlsx', data2)
     # write_to_excel("example.xlsx", "Sheet", data)
     # write_to_excel("example.xlsx", "Sheet1", data)
